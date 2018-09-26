bcaf049ee4fcb66e535b332eb9c10e5362235088
Your Name <you@example.com>
2018-09-26 10:32:02 -0400

    py

--- date.py
+++ date.py
@@ -2,6 +2,8 @@ import datetime
 
 now = datetime.datetime.now()
 
+print "Current date and time using str method of datetime object:"
+print str(now)
 print
 print "Current date and time using str method of datetime object:"
 print str(now)
